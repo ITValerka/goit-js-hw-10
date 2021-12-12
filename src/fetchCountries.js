f757e9f174ed54d51527fc9a2e9ac15d067b80bd
Valerii Mazurenko <Vmazurenko@gmail.com>
2021-12-12 19:07:51 +0200

    HW10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,23 +1,11 @@
-import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
-import countryCardTpl from './templates/country-cards.hbs';
-const DEBOUNCE_DELAY = 300;
-const countryCard = document.querySelector('.country-info');
-countryCard.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
-let countryName = '';
-
-function fetchCountries() {
-  fetch(
-    `https://restcountries.com/v3.1/name/${countryName}?fields=name,name.official,capital,population,flags,languages`,
-  )
-    .then(response => {
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+  ).then(response => {
+    if (response.ok) {
       return response.json();
-    })
-    .then(country => {
-      const markup = countryCardTpl(country);
-      countryCard.innerHTML = markup;
-    })
-    .catch(error => {
-      Notiflix.Notify.failure('Oops, there is no country with that name');
-    });
+    }
+    throw new Error();
+  });
 }
+export default { fetchCountries };
